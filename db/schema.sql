eb8c2473c071ce72570f0a5b21e9afce608be292
Alice Golter <inkflight@gmail.com>
2020-10-21 21:09:47 -0500

    actual tables and csv

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,31 @@
-DROP IS DATABASE EXISTS screamcast_db;
+DROP IF DATABASE EXISTS screamcast_db;
 CREATE DATABASE screamcast_db;
 USE screamcast_db;
 
-CREATE TABLE Movies (
+CREATE TABLE Movie_Data (
     id INT NOT NULL AUTO_INCREMENT,
-    Title VARCHAR(50) NULL,
-    Year INT NULL,
-    MovieDB_API_id VARCHAR(30) NULL,
-    PRIMARY KEY (id),
-    PRIMARY KEY (title),
-    FOREIGN KEY (MovieGenres_id)
+    Title VARCHAR(50) NOT NULL,
+    Year INT NOT NULL,
+    Genre1 VARCHAR(30) NOT NULL,
+    Genre2 VARCHAR(30) NULL,
+    Halloween BOOLEAN NOT NULL,
+    MovieDB_API_id VARCHAR(30) NOT NULL,
+    Poster_IMG 
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Genres (
+CREATE TABLE User_Info (
     id INT NOT NULL AUTO_INCREMENT,
-    GenreName VARCHAR(30) NULL,
-    PRIMARY KEY (GenreName)
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE MovieGenres (
-    FOREIGN KEY (Title),
-    FOREIGN KEY (GenreName)
+CREATE TABLE User_Reviews (
+    id INT NOT NULL AUTO_INCREMENT,
+    text_review VARCHAR(140) NULL,
+    spooky_rating DECIMAL(10,2)NOT NULL,
+    FOREIGN KEY User_Info(id),
+    FOREIGN KEY Movie_Data(id)
 );
 
 SELECT * FROM Movies;
