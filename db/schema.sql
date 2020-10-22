b17b4345d4daf27d08fd9710f499a36f9f325f57
Alice Golter <inkflight@gmail.com>
2020-10-21 22:17:08 -0500

    got my goddamn csv figured out

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-DROP IF DATABASE EXISTS screamcast_db;
+DROP DATABASE IF EXISTS screamcast_db;
 CREATE DATABASE screamcast_db;
 USE screamcast_db;
 
@@ -10,22 +10,22 @@ CREATE TABLE Movie_Data (
     Genre2 VARCHAR(30) NULL,
     Halloween BOOLEAN NOT NULL,
     MovieDB_API_id VARCHAR(30) NOT NULL,
-    Poster_IMG 
+    Poster_IMG VARCHAR(2083) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE User_Info (
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(30) NOT NULL,
-    password VARCHAR(30) NOT NULL
+    password VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE User_Reviews (
     id INT NOT NULL AUTO_INCREMENT,
     text_review VARCHAR(140) NULL,
-    spooky_rating DECIMAL(10,2)NOT NULL,
-    FOREIGN KEY User_Info(id),
-    FOREIGN KEY Movie_Data(id)
-);
-
-SELECT * FROM Movies;
+    spooky_rating DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY Movie_Data(id),
+    FOREIGN KEY User_Info(id)
+)
