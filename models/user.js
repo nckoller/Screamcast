62031d9c023cb47c735317542e6f32bd009e06d8
Nina Koller <nckoller@gmail.com>
2020-10-22 21:42:34 -0500

    added UserReview model, added associations to User model

--- models/user.js
+++ models/user.js
@@ -15,6 +15,12 @@ module.exports = function (sequelize, DataTypes) {
       allowNull: false,
     },
   });
+
+  //   User hasMany reviews
+  User.associate = function (models) {
+    User.hasMany(models.UserReview);
+  };
+
   // Check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
