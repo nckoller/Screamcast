62b241aa9d62eb6896b2840bc315d037680d1afd
Nina Koller <nckoller@gmail.com>
2020-10-24 23:09:59 -0500

    Edited movie model spooky rating to 1 digit, 1 decimal place. Added html and api routes for movie-list.handlebars and index.handlebars

--- models/movie.js
+++ models/movie.js
@@ -29,7 +29,7 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
     },
     spookyRating: {
-      type: DataTypes.DECIMAL(10, 2),
+      type: DataTypes.DECIMAL(1, 1),
       allowNull: true,
     },
     createdAt: {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,14 +6,26 @@ module.exports = function (app) {
   app.get('/', function (req, res) {
     res.render('index');
   });
+
   // Load login page
   app.get('/loginpage', function (req, res) {
     res.render('loginpage');
   });
+
   // Load movieDetailsPage
   app.get('/moviedetailspage', function (req, res) {
     res.render('moviedetailspage');
   });
 
-  app.get("/halloween")
+  // Load spooky move list
+  app.get('/spookymovies', function (req, res) {
+    // res.json({spooky: true})
+    res.render('movie-list', { spooky: true });
+  });
+
+  // Load halloween movie list
+  app.get('/halloweenmovies', function (req, res) {
+    // res.json({spooky: true})
+    res.render('movie-list', { halloween: true });
+  });
 };
